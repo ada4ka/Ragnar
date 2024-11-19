8ac39384d7afdcc4a856734be27afef15e7e7abd
ariadna tsolka <ariadna20080609@gmail.com>
2024-11-19 18:53:33 +0300

    finished burger menu

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+const navItem = document.querySelector('.header-nav-item')
+const burgerMenu = document.querySelector('.header-nav-burger')
+const navBurgerLine = document.querySelectorAll('.nav-burger-line')
+
+let count = 1
+
+burgerMenu.addEventListener('click',function(){
+    count++
+    if(count % 2 == 0){
+      navItem.style.transform = 'translateX(0%)'
+      navBurgerLine.forEach(function(element,index){
+          if(index == 0 || index == 2){
+              element.style.width = '12.5px'
+              element.style.background = '#8856D9'
+          }
+          else{
+              element.style.width = '25px'
+              element.style.background = '#8856D9'
+          }
+      })
+    }else{
+      navItem.style.transform = 'translateX(-150%)'
+      navBurgerLine.forEach(function(element,index){
+        if(index == 0 || index == 2){
+            element.style.width = '25px'
+            element.style.background = 'white'
+        }
+        else{
+            element.style.width = '12.5px'
+            element.style.background = 'white'
+        }
+    })
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
